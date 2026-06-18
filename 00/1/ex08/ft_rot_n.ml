becfef16a1c381ed5fa27d6db65189e05590e351
sim1gariglio <simone.gariglio1@gmail.com>
2026-06-18 16:54:27 +0200

    corretto ex08

--- 00/1/ex08/ft_rot_n.ml
+++ 00/1/ex08/ft_rot_n.ml
@@ -8,11 +8,15 @@ let ft_rot_n n str =
       c
   ) str
 
+(*
+
 let () =
-  print_endline (ft_rot_n 1 "abcdefghijklmnopqrstuvwxyz");
-  print_endline (ft_rot_n 13 "abcdefghijklmnopqrstuvwxyz");
-  print_endline (ft_rot_n 42 "0123456789");
-  print_endline (ft_rot_n 2 "OI2EAS67B9");
-  print_endline (ft_rot_n 0 "Damned !");
-  print_endline (ft_rot_n 42 "");
-  print_endline (ft_rot_n 1 "NBzlk qnbjr !")
+print_endline (ft_rot_n 1 "abcdefghijklmnopqrstuvwxyz");
+print_endline (ft_rot_n 13 "abcdefghijklmnopqrstuvwxyz");
+print_endline (ft_rot_n 42 "0123456789");
+print_endline (ft_rot_n 2 "OI2EAS67B9");
+print_endline (ft_rot_n 0 "Damned !");
+print_endline (ft_rot_n 42 "");
+print_endline (ft_rot_n 1 "NBzlk qnbjr !")
+
+*)
